047f2bf895a51b2b6d8afe1c4a130d5f0d43b739
SachJay <jayaweerasachin@gmail.com>
2021-09-05 17:02:07 -0400

    backend - adds backend cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+var fs = require("fs");
 
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({
@@ -9,8 +11,29 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/:name', (req, res) => {
+app.get('/:name', (req, res, next) => {
     let name = req.params.name;
+
+    fs.readFile("C:/Users/SachJ/Downloads/accountactivity - accountactivity.csv", 'utf8', function(err, buf) {
+        if (err) {
+            return console.log(err);
+        }
+
+        var data = buf.split('\n');
+        data = data.map(x => {
+            var interaction = x.split(",");
+            return {
+                date: interaction[0],
+                name: interaction[1],
+                cost: interaction[2],
+                income: interaction[3],
+                amount: interaction[4],
+                catagory: 0
+            }
+        });
+
+        console.log(JSON.stringify(data));
+    });
 
     res.json({
         message: `Hello ${name}`
