6e6bd26d1c43f9d7e97bc1b35ee534c501291d08
SachJay <jayaweerasachin@gmail.com>
2021-09-13 22:47:08 -0400

    frontend - created components for select catagory

---
+++ frontend/src/components/SelectCatagory.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>Select a Catagory</h1>
+
+    <ul>
+        <li>Finance</li>
+        <li>Tea</li>
+        <li>Milk</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+    },
+    params: {
+    },
+    methods: {
+     
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
